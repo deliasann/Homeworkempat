ac646445bf41b55b93056edc4adca1d94563bfc4
Adelia Sannomiya <adelia.sannomiya@gmail.com>
2022-03-28 14:56:03 +0700

    commit six

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PlaylistContainer from './components/PlaylistContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Create Playlist</h1>
+      <PlaylistContainer />
+    </div>
+  );
+}
+
+export default App;
